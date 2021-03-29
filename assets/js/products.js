580039540f9bb6f283758ff8fb5c4f6e2fb313b6
Miroslav Mirchev <mirchev1977@yahoo.com>
2021-03-29 04:52:44 +0300

    extracted html into products.vue file

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,15 +1,4 @@
 import {compile, createApp} from 'vue';
+import App from './pages/products';
 
-
-const template = '<h1>Hello {{ firstName }}! Is this cooler?</h1>';
-
-createApp({
-    data() {
-        return {
-            firstName: 'Miroslav',
-        };
-    },
-    render() {
-        return compile(template)(this.$.ctx);
-    },
-}).mount('#app');
+createApp(App).mount('#app');
